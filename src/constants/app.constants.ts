8816040e20b788e051e40f73998cc782bbacfb6a
Niccolò Valente <niccolo.valente.biz@gmail.com>
2025-07-29 15:14:48 +0200

    fix for deploy

--- src/constants/app.constants.ts
+++ src/constants/app.constants.ts
@@ -1,15 +1,12 @@
 import { AccessLevelsEnum, RolesEnum } from "@/src/enums/roles.enums";
 import { isMocked } from "@/src/utils/envs.utils";
-import { isServer } from "../utils/common.utils";
 
 export const AppConstants = {
   title: "Vigila App",
   description: "Is a simple base-app based on Next.js and Supabase",
   hostUrl: isMocked
     ? "http://localhost:3000"
-    : isServer
-      ? "https://todo-define-url.vercel.app"
-      : window.location.origin,
+    : "https://todo-define-url.vercel.app",
   defaultUserRole: RolesEnum.CONSUMER,
   defaultUserLevel: AccessLevelsEnum.BASE,
 };
