3987d0dcd053a8fea390a688afdca06303f5b23d
Niccolò Valente <niccolo.valente.biz@gmail.com>
2025-08-02 12:27:18 +0200

    removed unused types

--- src/types/unit.types.ts
+++
@@ -1,9 +0,0 @@
-export interface UnitI {
-  id: string;
-  userId: string;
-  name?: string;
-  description?: string;
-  active?: boolean;
-  createdAt?: Date | string;
-  updatedAt?: Date | string;
-}
