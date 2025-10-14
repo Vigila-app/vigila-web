fe862eaaa0617026dd32609d51ee830e91013e6b
Niccolò Valente <niccolo.valente.biz@gmail.com>
2025-10-14 17:41:32 +0200

    minor fix

--- app/vigil/tabs/recensioni.tsx
+++ app/vigil/tabs/recensioni.tsx
@@ -123,7 +123,6 @@ export default function RecensioniTab({
               isEditable={true}
               showEditInline={true}
               simplified={simplified}
-              simplified={simplified}
             />
           ))
         ) : (
