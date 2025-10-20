e2951d0a83478fd577d37c298689d42954e3d70c
ManueLomaglio <manuelomaglio@gmail.com>
2025-10-20 15:52:57 +0200

    Fix/onboarding vigil (#53)
    * fix: commentato/eliminato codice per creazione servizi, migliorato design catalogo servizi
    
    * fix: aumentato il testo permesso per le informazioni

--- components/onboarding/vigil/onboardComponent_Vigil.tsx
+++ components/onboarding/vigil/onboardComponent_Vigil.tsx
@@ -134,18 +134,20 @@ const VigilOnboardComponent = () => {
   };
 
   return (
-    <div className="w-full px-4 pt-8 pb-4">
+    <div className="w-full max-w-full mx-auto px-4 pt-8 pb-4">
       <Card>
         <div className="">
           <section className="flex flex-col items-center gap-2">
-            <p className="font-semibold text-[28px] text-vigil-orange">Iniziamo a conoscerti</p>
+            <p className="font-semibold text-[28px] text-vigil-orange">
+              Iniziamo a conoscerti
+            </p>
             <span className="font-normal text-center text-lg break-normal whitespace-normal">
               Raccontaci qualcosa di te per iniziare questa bella avventura
             </span>
           </section>
           <form
             onSubmit={handleSubmit(onSubmit)}
-            className="w-full mx-auto max-w-lg space-y-10 py-8 px-2">
+            className="w-full mx-auto max-w-lg space-y-10 py-8 px-2 ">
             <Controller
               name="birthday"
               control={control}
@@ -227,24 +229,6 @@ const VigilOnboardComponent = () => {
               )}
             />
 
-            {/* <Controller
-              name="cap"
-              control={control}
-              rules={{ required: true, ...FormFieldType.CAP }}
-              render={({ field }) => (
-                <Input
-                  {...field}
-                  label="CAP"
-                  role={role}
-                  placeholder="Inserisci il tuo CAP di residenza"
-                  required
-                  error={errors.cap}
-                  minLength={FormFieldType.CAP.minLength}
-                  maxLength={FormFieldType.CAP.maxLength}
-                  aria-invalid={!!errors.cap}
-                />
-              )}
-            /> */}
             {/* TODO controller per il campo adresses con l'auto completamento tramite il compoenente mappe */}
             <Controller
               name="addresses"
@@ -340,7 +324,7 @@ const VigilOnboardComponent = () => {
             <Controller
               name="information"
               control={control}
-              rules={{ required: true, maxLength: 400 }}
+              rules={{ required: true, maxLength: 650 }}
               render={({ field }) => (
                 <TextArea
                   {...field}
