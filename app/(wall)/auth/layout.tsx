6ffe9c8d41c26f9ceb2de10a11373d7bfec81aea
Niccolo Valente <niccolo.valente.biz@gmail.com>
2026-01-13 17:31:41 +0100

    Feat/google login (#84)
    * feat: sign in google
    
    * feat: redirect page e auth service fix
    
    * feat: redirect e controlli di accesso a pagina di scelta role
    
    * fix alla white list dell'utente senza ruolo
    
    * feat: api per completamento registrazione e funzione utils
    
    * feat: componente per termini e collegamento con api
    
    * fix:design and redirect control
    
    * fix: auth services
    
    * fix callback, enums, api costant, api post
    
    * hide google button to TEST
    
    * fix completeGoogle API
    
    * fix google login redirect
    
    ---------
    
    Co-authored-by: Manuel Lomaglio <manuelomaglio@gmail.com>

--- app/(wall)/auth/layout.tsx
+++ app/(wall)/auth/layout.tsx
@@ -6,6 +6,7 @@ import { useUserStore } from "@/src/store/user/user.store";
 import { isDev } from "@/src/utils/envs.utils";
 import { redirect, usePathname } from "next/navigation";
 import Script from "next/script";
+import { useEffect } from "react";
 
 export default function AuthLayout({
   children,
@@ -17,26 +18,30 @@ export default function AuthLayout({
 
   const allowedAuthRoutes = [Routes.registrationRole.url];
 
-  const isOnboardingPage = allowedAuthRoutes.some((route) =>
-    pathname?.includes(route)
-  );
-  if (user?.id) {
-    const hasRole = !!user.user_metadata?.role;
+  useEffect(() => {
+    const isOnboardingPage = allowedAuthRoutes.some((route) =>
+      pathname?.includes(route)
+    );
+    if (user?.id) {
+      const hasRole = !!user.user_metadata?.role;
 
-    if (hasRole) {
-      redirect(Routes.home.url);
-    }
+      if (hasRole) {
+        redirect(Routes.home.url);
+      }
 
-    if (!isOnboardingPage) {
-      redirect(Routes.home.url);
+      if (!isOnboardingPage) {
+        redirect(Routes.home.url);
+      }
     }
-  }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user, pathname]);
 
   return (
     <>
       {!isDev ? (
         <Script
-          src={`https://www.google.com/recaptcha/enterprise.js?render=${SupabaseConstants.appCheckPublicKey}&badge=bottomleft`}></Script>
+          src={`https://www.google.com/recaptcha/enterprise.js?render=${SupabaseConstants.appCheckPublicKey}&badge=bottomleft`}
+        ></Script>
       ) : null}
       {children}
     </>
