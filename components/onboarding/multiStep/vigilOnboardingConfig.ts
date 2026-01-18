c4c94131650319fd6ad1903c44feae18f4ddfd3d
Manuel Lomaglio <manuelomaglio@gmail.com>
2026-01-18 23:32:42 +0100

    fix vigil config

--- components/onboarding/multiStep/vigilOnboardingConfig.ts
+++ components/onboarding/multiStep/vigilOnboardingConfig.ts
@@ -21,7 +21,7 @@ export const createVigilOnboardingConfig = (
   onComplete: (data: Record<string, any>) => Promise<void>
 ): OnboardingFlowConfig => ({
   role: RolesEnum.VIGIL,
-  initialStepId: "service-areas",
+  initialStepId: "welcome",
   onComplete,
   //Nota: ho modificare leggermente il primo step rispetto a figma: alcune info le abbiamo dal form di registrazione
   steps: [
